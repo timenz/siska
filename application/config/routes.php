bdfe98396384610a6b6ee43a1bad6a67f5db87a0
tmn <tmn@tmn-pc>
2013-02-23 00:30:22 +0700

    tmn

--- application/config/routes.php
+++ application/config/routes.php
@@ -40,7 +40,7 @@
 
 $route['default_controller'] = "home";
 $route['404_override'] = '';
-
+$route['(:any)'] = 'home/index';
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
