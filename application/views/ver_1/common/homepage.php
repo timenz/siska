149fa780edbd4b33c7cb611846b5ca7193ac3e23
timenz <ngatimen@yahoo.com>
2013-05-03 20:48:03 +0700

    update sebelum push banyak yang terlewat ternyata

---
+++ application/views/ver_1/common/homepage.php
@@ -0,0 +1,110 @@
+
+<!--Page contetn-->
+<div class="span8">
+    <section>
+        <div class="row">
+            <div class="span6">
+                <div class="blog_item">
+                    <div class="blog_head">
+                        <h3><a href="blog_single.html">Blog post Title </a></h3>
+                    </div>
+                    <div class="view view-first">
+                        <a href="{assets_url}img/gal/6.jpg" rel="prettyPhoto"><img src="{assets_url}img/gal/6.jpg" alt="" /></a>
+                        <div class="mask">
+                            <a href="{assets_url}img/gal/6.jpg" rel="prettyPhoto" class="info"><img src="{assets_url}img/plus.png" alt="Hanging Note Sign Psd" /></a>
+                            <a href="blog_single.html" class="link"><img src="{assets_url}img/link.png" alt="Visit link" /></a>
+                        </div>
+                    </div>
+                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'.<br><br><a class="btn btn-small" href="blog_single.html">Read More</a></p>
+                </div>
+            </div>
+            <div class="span2">
+                <div class="ddate">
+                    <h5><i class="icon-calendar"></i> 25 April / 2012</h5>
+                    <div class="firstA"></div>
+                </div>
+                <div class="meta">
+                    <span><strong><i class="icon-list-alt"></i> Category:</strong> <a href="#">Porgramming</a></span>
+                    <span><strong><i class="icon-user"></i> Author:</strong> <a href="#">OrangeIdea</a></span>
+                    <span><strong><i class="icon-comment"></i> Comments:</strong> <a href="#">24</a></span>
+                </div>
+                <hr>
+                <span class="share">Share options: </span>
+                <div class="addthis_toolbox addthis_default_style ">
+                    <a class="addthis_counter addthis_pill_style"></a>
+                </div>
+                <hr>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="row">
+            <div class="span6">
+                <div class="blog_item">
+                    <div class="blog_head">
+                        <h3><a href="blog_single.html">Cassidy - Erevan Tusk</a></h3>
+                    </div>
+                    <div class="row">
+                        <iframe src="http://player.vimeo.com/video/40221513?color=ffffff" class="span6" height="270" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+                    </div>
+                    <p>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'.<br><br><a class="btn btn-small" href="blog_single.html">Read More</a></p>
+                </div>
+            </div>
+            <div class="span2">
+                <div class="ddate">
+                    <h5><i class="icon-calendar"></i> 20 April / 2012</h5>
+                    <div class="firstA"></div>
+                </div>
+                <div class="meta">
+                    <span><strong><i class="icon-list-alt"></i> Category:</strong> <a href="#">Video</a></span>
+                    <span><strong><i class="icon-user"></i> Author:</strong> <a href="#">OrangeIdea</a></span>
+                    <span><strong><i class="icon-comment"></i> Comments:</strong> <a href="#">15</a></span>
+                </div>
+                <hr>
+                <span class="share">Share options: </span>
+                <div class="addthis_toolbox addthis_default_style ">
+                    <a class="addthis_counter addthis_pill_style"></a>
+                </div>
+                <hr>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="row">
+            <div class="span6">
+                <div class="blog_item">
+                    <div class="blog_head">
+                        <h3><a href="blog_single.html">Audio pleeer PSD</a></h3>
+                    </div>
+                    <div class="view view-first">
+                        <a href="{assets_url}img/gal/5.jpg" rel="prettyPhoto"><img src="{assets_url}img/gal/5.jpg" alt="" /></a>
+                        <div class="mask">
+                            <a href="{assets_url}img/gal/5.jpg" rel="prettyPhoto" class="info"><img src="{assets_url}img/plus.png" alt="Hanging Note Sign Psd" /></a>
+                            <a href="blog_single.html" class="link"><img src="{assets_url}img/link.png" alt="Visit link" /></a>
+                        </div>
+                    </div>
+                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'.<br><br><a class="btn btn-small" href="blog_single.html">Read More</a></p>
+                </div>
+            </div>
+            <div class="span2">
+                <div class="ddate">
+                    <h5><i class="icon-calendar"></i> 15 April / 2012</h5>
+                    <div class="firstA"></div>
+                </div>
+                <div class="meta">
+                    <span><strong><i class="icon-list-alt"></i> Category:</strong> <a href="#">Design</a></span>
+                    <span><strong><i class="icon-user"></i> Author:</strong> <a href="#">OrangeIdea</a></span>
+                    <span><strong><i class="icon-comment"></i> Comments:</strong> <a href="#">24</a></span>
+                </div>
+                <hr>
+                <span class="share">Share options: </span>
+                <div class="addthis_toolbox addthis_default_style ">
+                    <a class="addthis_counter addthis_pill_style"></a>
+                </div>
+                <hr>
+            </div>
+        </div>
+    </section>
+
+</div>
+<!--/Page contetn-->
---
+++ application/views/ver_1/common/slider.php
@@ -0,0 +1,26 @@
+<!--SLIDER AREA-->
+<div class="slider_area">
+    <div class="container">
+        <div class="row">
+            <div class="span12 mainslider">
+                <div class="flexslider">
+                    <ul class="slides">
+                        <li>
+                            <img src="{assets_url}img/slide1.jpg" />
+                        </li>
+                        <li>
+                            <a href="http://flex.madebymufffin.com"><img src="{assets_url}img/slide1-1.jpg" /></a>
+                        </li>
+                        <li>
+                            <img src="{assets_url}img/slide1-2.jpg" />
+                        </li>
+                        <li>
+                            <img src="{assets_url}img/slide1-3.jpg" />
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/SLIDER AREA-->
