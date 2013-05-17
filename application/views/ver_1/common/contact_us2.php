865b8626db719b248f9ce359422440f0b5c86e0e
timen <ngatimen@yahoo.com>
2013-05-17 20:35:12 +0700

    Merge branch 'develop_ida' of https://github.com/timenz/siska into develop
    Conflicts:
    	.idea/workspace.xml

---
+++ application/views/ver_1/common/contact_us2.php
@@ -0,0 +1,37 @@
+
+<!--Page contetn-->
+<div class="span8">
+    <section>
+        <div class="row">
+                <div class="span8">
+                    <div class="well">
+                        <h4><span class="colored">///</span> Fontact information</h4>
+                        <p>Valera is designed to help people of all skill levels designer or developer, huge nerd or early beginner. Use it as a complete kit or use to start something more complex.</p>
+                        <hr>
+                        <span><strong class="colored"> Aress:</strong> 123456 Street Name, Los Angeles</span>
+                        <br>
+                        <span><strong class="colored">Phone:</strong> (1800) 765-4321</span>
+                        <br>
+                        <span><strong class="colored">Fax:</strong> (1800) 765-4321</span>
+                        <br>
+                        <span><strong class="colored">Website:</strong> http://yoursitename.com</span>
+                        <br>
+                        <span><strong class="colored">Email:</strong> info@yoursitename.com</span>
+                    </div>
+                </div>
+                <div class="span8">
+                    <h3><span class="colored">///</span> Feel free to contact us</h3>
+                    <div id="note"></div>
+                    <div id="fields">
+                        <form class="form" id="ajax-contact-form" action="javascript:alert('Was send!');">
+                            <input type="text" name="name" class="span4" style="margin-right:25px;" placeholder="Name" />
+                            <input class="span4" name="email" placeholder="Email" />
+                            <textarea type="text" name="message" placeholder="Message" rows="8" class="span8"></textarea>
+                            <button type="submit" class="btn btn-success">Send message</button>
+                        </form>
+                    </div>
+                </div>
+        </div>
+    </section>
+</div>
+<!--/Page contetn-->
