70d73698d9a287970170b79bc214306c2356c9f0
timen <ngatimen@yahoo.com>
2013-06-11 17:16:39 +0700

    halaman kemahasiswaan part 2 sampai cetak ktm

---
+++ application/views/ver_1/mahasiswa/cetak_ktm.php
@@ -0,0 +1,17 @@
+
+<!--Page contetn-->
+<div class="span8">
+    <section>
+        <div class="row">
+            <div class="span8">
+                <div class="blog_item">
+                    <h2>Halaman Cetak KTM Sementara </h2>
+                    <p>Bagi mahasiswa baru silakan cetak KTM dari link di bawah ini, dan mintakan kepada administrasi untuk meminta stempel.<br><br>
+
+                        <a class="btn btn-small" target="_blank" href="{base_index}post/mahasiswa/cetak_ktm">Cetak KTM</a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<!--/Page contetn-->
