5c420880fd85c6e946252ac0cba15c10b8b92a8a
widianik <pinguin.kecil.cute@gmail.com>
2013-06-12 17:02:22 +0700

    tampilan khs mahasiswa...

--- application/views/ver_1/khs/list_nilai.php
+++ application/views/ver_1/khs/list_nilai.php
@@ -1,5 +1,45 @@
+<div class="span8">
+    <section>
+        <div class="row">
+            <div class="span8">
+                <div class="blog_item">
+                    <h3 class="heading">KARTU HASIL STUDY</h3>
+                </div>
+                <div>
+                    SELAMAT DATANG, Kartika
+                 </div>
 
-<!--Page contetn-->
+            <div>
+                <table class="table table-striped">
+                    <thead><tr>                <th>#</th>
+                        <th>MATA KULIAH</th>
+                        <th>SKS</th>
+                        <th>NILAI HURUF</th>
+                        <th>NILAI ANGKA</th>
+                        <th>SKS x NA</th>
+                    </tr></thead>
+                    <tbody>            <tr>                    <td>1</td>
+                        <td>FISIKA I
+                        </td>
+                        <td>4</td>
+                        <td>A</td>
+                        <td>4.00</td>
+                        <td>16</td>
+                    </tr>
+                    </tbody>
+                </table>
+                <b>TOTAL SKS = 4<br/>IPK = 4</b><br/>Jika ada kekeliruan dapat konfirmasi ke Tata Usaha
+            </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+
+
+
+<!--Page contetn--
 <div class="span8">
     <section>
         <div class="row">
