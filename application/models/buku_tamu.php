aaa0f3ad60a7012c6c5b713ec0121d79c1c31a34
timen <ngatimen@yahoo.com>
2013-06-14 20:06:02 +0700

    goin hibernate

--- application/models/buku_tamu.php
+++ application/models/buku_tamu.php
@@ -1,5 +1,7 @@
 <?php
 
+// file application/models/bukutamu.php
+
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 class buku_tamu extends CI_Model {
     public function __construct() {
